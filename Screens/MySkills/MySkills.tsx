1046603ae962ffb07e062ba1cd6a00e9567b782d
Rodrigo Siqueira <rodrigomsiqq@gmail.com>
2024-06-06 14:32:27 -0300

    desenvolvimento e final do app realizado

--- Screens/MySkills/MySkills.tsx
+++ Screens/MySkills/MySkills.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { Linking, TouchableOpacity, View } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 import { styles } from './StyleMySkills';
+import { Profile } from '../../components/ProfilePic/ProfilePic';
+import Linkedin from '../../components/SocialButtons/Linkedin';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function MySkills() {
+export default function MySkills({navigation}:any) {
+  const handleOpenLink2 = async(url:string)=>{
+    try{
+      await Linking.openURL(url)
+    } catch(error){
+      console.error('erro no link', error)
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-        <Text>works</Text>
+        <Profile/>
+        <View style={styles.galerry}>
+        <FontAwesome5 name="react" size={40} color="purple" />
+        <FontAwesome5 name="html5" size={40} color="purple" />
+        <FontAwesome5 name="css3" size={40} color="purple" />
+        <MaterialCommunityIcons name="tailwind" size={40} color="purple" />
+        <FontAwesome5 name="js" size={40} color="purple" />
+        <FontAwesome5 name="docker" size={40} color="purple" />
+        <FontAwesome5 name="python" size={40} color="purple" />
+        <FontAwesome5 name="linux" size={40} color="purple" />
+        </View>
+        <TouchableOpacity style={styles.SocialButtons2}
+        onPress={()=>handleOpenLink2('mailto:rodrigomsiqq@gmail.com')}     
+        >
+          <Linkedin descript='Entre em Contato'/>
+        </TouchableOpacity>
     </View>
   );
 }
--- Screens/MySkills/StyleMySkills.tsx
+++ Screens/MySkills/StyleMySkills.tsx
@@ -2,6 +2,29 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor:'#da0000'
-  }
+    flex:1,
+    backgroundColor:'#000000',
+    alignItems:'center',
+    justifyContent:'flex-start',
+    paddingTop:50
+  },
+  galerry:{
+    padding:10,
+    marginTop:20,
+    alignItems:'center',
+    justifyContent:'center',
+    flexDirection:'row',
+    flexWrap:'wrap',
+    gap:20,
+
+  },
+  SocialButtons2:{
+    alignItems:"center",
+    backgroundColor:"#ffffff",
+    borderColor:"grey",
+    borderWidth:2,
+    width:140,
+    padding:8,
+    margin:10
+  },
 });
